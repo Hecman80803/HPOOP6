ebce67fb05e43f7083d3e2cc9f92266ce4a0a6d8
Hecman80803 <147789917+Hecman80803@users.noreply.github.com>
2023-10-13 00:32:58 -0600

    Creacion de la practica

---
+++ src/poop6/POOP6.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poop6;
+
+/**
+ *
+ * @author hecman, Damian, Jhonny
+ */
+public class POOP6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        Persona persona = new Persona(19, "Colonia Emiliano Zapata", "Hector", "Cisneros");
+        persona.hablar("Hola, soy una persona");
+    }
+    
+}
